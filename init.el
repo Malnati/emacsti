75e1a5018a9815b881e40a31d290fed977708d5b
Ricardo Malnati <ricardomalnati@gmail.com>
2020-11-23 11:43:50 -0300

    fixing which-key setup

--- init.el
+++ init.el
@@ -19,23 +19,30 @@
 ;;--------------------------------------------------------------------
 ;; Custom defined variables
 ;;--------------------------------------------------------------------
-(setq emacsti-homedir             "~/"                                            )
-(message (concat "Eval global var emacsti-homedir                  → " emacsti-homedir            ))
-
-(setq emacsti-emacsdir            (concat emacsti-homedir  ".emacs.d/"                        ))
-(message (concat "Eval global var emacsti-emacsdir                 → " emacsti-emacsdir           ))
-
-(setq emacsti-dotemacslib         (concat emacsti-emacsdir "org-dotemacs/org-dotemacs.el"                  ))
-(message (concat "Eval global var emacsti-dotemacslib              → " emacsti-dotemacslib        ))
-
-(setq emacsti-dotemacsdir         (concat emacsti-emacsdir "dotfiles/"                        ))
-(message (concat "Eval global var emacsti-dotemacsdir              → " emacsti-dotemacsdir        ))
-
-(setq emacsti-dotemacsfile        (concat emacsti-dotemacsdir  "dotemacs.org"                 ))
-(message (concat "Eval global var emacsti-dotemacsfile             → " emacsti-dotemacsfile       ))
-
-(setq emacsti-dotemacsfile-index       (concat emacsti-dotemacsdir ".custom-index.org"           ))
-(message (concat "Eval global var emacsti-dotemacsfile-index       → " emacsti-dotemacsfile-index      ))
+(setq emacsti-homedir "~/")
+(message
+ (concat "Eval global var emacsti-homedir            → " emacsti-homedir))
+(setq emacsti-emacsdir
+      (concat emacsti-homedir  ".emacs.d/"))
+(message
+ (concat "Eval global var emacsti-emacsdir           → " emacsti-emacsdir))
+(setq emacsti-dotemacslib
+      (concat emacsti-emacsdir "org-dotemacs/org-dotemacs.el"))
+(message
+ (concat "Eval global var emacsti-dotemacslib        → " emacsti-dotemacslib))
+(setq emacsti-dotemacsdir
+      (concat emacsti-emacsdir "dotfiles/")
+      )
+(message
+ (concat "Eval global var emacsti-dotemacsdir        → " emacsti-dotemacsdir))
+(setq emacsti-dotemacsfile
+      (concat emacsti-dotemacsdir  "dotemacs.org"))
+(message
+ (concat "Eval global var emacsti-dotemacsfile       → " emacsti-dotemacsfile))
+(setq emacsti-dotemacsfile-index
+      (concat emacsti-dotemacsdir ".custom-index.org"))
+(message
+ (concat "Eval global var emacsti-dotemacsfile-index → " emacsti-dotemacsfile-index))
 
 (message "\nAll of custom variables loaded!")
 
@@ -66,6 +73,48 @@
 ;;
 (message "All packages initilized and selected!")
 
+;; Frame, window, toolbar, menu custom-set-variables was added by Custom.
+(message "\n Custom set for Frame, window, toolbar, menu loading... \n")
+;; remove scroll bar
+;; This variable controls whether and where to put vertical 
+;; scroll bars in all frames. The possible values 
+;; are nil for no scroll bars, left to put scroll 
+;; bars on the left and right to put scroll bars on the right. 
+;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Scroll-Bars.html
+(scroll-bar-mode -1)
+(message "Removed scrolls!")
+;; remove toolbar
+(tool-bar-mode -1)
+(message "Removed tool bar!")
+(column-number-mode t)
+(message "Showing colunm number!")
+;; https://emacsredux.com/blog/2015/01/18/customizing-the-fringes/
+;; make both fringes 12 pixels
+(fringe-mode 12)
+(message "Fringes sized to 12px!")
+(setq display-line-numbers t)
+(message "Showing line numbers!")
+(global-display-line-numbers-mode t)
+(message "Showing line numbers globally!")
+(window-divider-mode t)
+(message "Window divider ON!")
+(setq buffers-menu-buffer-name-length 64)
+(message "Buffers menu lenght sized to 64!")
+(setq buffers-menu-max-size 32)
+(message "Buffers menu sized to 32!")
+(setq buffers-menu-show-directories t)
+(message "Buffers menu show directories ON!")
+;; (setq default-frame-alist
+;;    (quote
+;;          ((tool-bar-position . none)
+;;           (bottom-divider-width . 1)
+;;           (right-divider-width . 1))))
+(setq doc-view-continuous t)
+(setq display-buffer-reuse-frames t)
+;;(tool-bar-position (quote top))
+(setq window-divider-default-bottom-width 1)
+(setq window-divider-default-right-width 1)
+(message "\n Custom set for Frame, window, toolbar, menu done! \n")
 ;;--------------------------------------------------------------------
 ;; Loading org-dotemacs
 ;;--------------------------------------------------------------------
@@ -87,33 +136,3 @@
 ;;--------------------------------------------------------------------
 ;; End .emacs code
 ;;--------------------------------------------------------------------
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "gray7" :foreground "#f6f3e8" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 86 :width normal :foundry "PfEd" :family "DejaVu Sans Mono"))))
- '(beacon-fallback-background ((t (:background "dim gray"))))
- '(button ((t (:background "#333333" :foreground "white"))))
- '(cursor ((t (:background "gray25" :foreground "white"))))
- '(highlight ((t (:background "gray5" :foreground "#ffffff" :underline "dim gray"))))
- '(hl-line ((t (:background "black" :foreground "gainsboro"))))
- '(isearch ((t (:background "dark orange" :foreground "black"))))
- '(lazy-highlight ((t (:background "dark orange" :foreground "black"))))
- '(line-number ((t (:inherit (shadow default) :foreground "gray15"))))
- '(line-number-current-line ((t (:foreground "dark orange"))))
- '(match ((t (:background "orange" :foreground "black"))))
- '(mode-line ((t (:background "black" :foreground "gainsboro"))))
- '(mode-line-emphasis ((t (:background "black" :foreground "yellow" :weight bold))))
- '(mode-line-highlight ((t (:background "black" :foreground "yellow" :box (:line-width 2 :color "gold" :style released-button)))))
- '(mode-line-inactive ((t (:inherit default :background "#444444" :foreground "gray10"))))
- '(package-status-incompat ((t (:foreground "red"))))
- '(query-replace ((t (:background "dark orange" :foreground "black"))))
- '(rectangle-preview ((t (:inherit region :background "steel blue" :foreground "black"))))
- '(show-paren-match ((t (:background "dark orange" :foreground "black"))))
- '(show-paren-mismatch ((t (:background "red" :foreground "yellow"))))
- '(strokes-char ((t (:background "gray2"))))
- '(tooltip ((t (:background "black" :foreground "dark olive green"))))
- '(tty-menu-disabled-face ((t (:background "black" :foreground "lightgray"))))
- '(tty-menu-enabled-face ((t (:background "black" :foreground "yellow" :weight bold))))
- '(window-divider-first-pixel ((t (:foreground "gray10")))))
