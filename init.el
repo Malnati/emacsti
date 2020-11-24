0d01f7d1617b4d1044840c1598357df0636b70e3
Ricardo Malnati <35036961+Malnati@users.noreply.github.com>
2020-11-23 23:51:25 -0300

    company and nord theme
    * adding flycheck package
    
    * adding lsp-ui package
    
    * adding yasnippet package
    
    * adding Yasnippet custom setup loading
    
    * adding helm-company package
    
    * adding aggressive-indent package
    
    * adding aggrecive-mode for elisp and CSS
    
    * fixing .custom-set-variables
    
    * fixing .custom-set-variables
    
    * fixing .custom-set-variables
    
    * fixing .custom-set-variables
    
    * fixing .custom-set-variables
    
    * fixing .custom-set-variables
    
    * fixing .custom-set-variables
    
    * fixing .custom-set-variables
    
    * fixing .custom-faces
    
    * adding nord theme
    
    * projectile to gitignore

--- init.el
+++ init.el
@@ -58,6 +58,21 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(add-hook (quote after-init-hook) t)
+ '(aggressive-indent-mode nil t)
+ '(beacon-color "gold")
+ '(beacon-fallback-background ((t (:background "dim gray"))))
+ '(beacon-mode t)
+ '(custom-safe-themes
+   (quote
+    ("37768a79b479684b0756dec7c0fc7652082910c37d8863c35b702db3f16000f8" default)))
+ '(delete-selection-mode 1)
+ '(desktop-base-file-name "emacs-desktop")
+ '(desktop-save t)
+ '(desktop-save-mode t)
+ '(global-flycheck-mode nil)
+ '(global-visual-line-mode t)
+ '(org-startup-with-beamer-mode t)
  '(package-archives
    (quote
     (("marmalade" . "http://marmalade-repo.org/packages/")
@@ -69,7 +84,10 @@
  '(package-enable-at-startup t)
  '(package-selected-packages
    (quote
-    (which-key projectile lsp-javacomp lsp-java ## hydra company-lsp company helm-descbinds helm org org-gnome org-dotemacs org-web-tools org-ref org-re-reveal org-presie org-projectile-helm org-projectile org-pomodoro org-pdfview org-page org-mime org-make-toc org-linkany org-index org-edna org-elisp-help org-kanban org-dp org-beautify-theme org-alert plantuml-mode flycheck-plantuml ox-reveal org-re-reveal-ref oer-reveal graphviz-dot-mode ob-dar magit magithub zoom doom-themes))))
+    (nord-theme all-the-icons-ivy-rich all-the-icons-ivy all-the-icons-ibuffer all-the-icons-gnus all-the-icons-dired neotree aggressive-indent helm-company yasnippet lsp-ui flycheck which-key projectile lsp-javacomp lsp-java ## hydra company-lsp company helm-descbinds helm org org-gnome org-dotemacs org-web-tools org-ref org-re-reveal org-presie org-projectile-helm org-projectile org-pomodoro org-pdfview org-page org-mime org-make-toc org-linkany org-index org-edna org-elisp-help org-kanban org-dp org-beautify-theme org-alert plantuml-mode flycheck-plantuml ox-reveal org-re-reveal-ref oer-reveal graphviz-dot-mode ob-dar magit magithub zoom doom-themes)))
+ '(which-key-mode t)
+ '(which-key-setup-side-window-right-bottom nil)
+ '(yas-global-mode nil))
 ;;
 (message "All packages initilized and selected!")
 
@@ -104,16 +122,76 @@
 (message "Buffers menu sized to 32!")
 (setq buffers-menu-show-directories t)
 (message "Buffers menu show directories ON!")
-;; (setq default-frame-alist
-;;    (quote
-;;          ((tool-bar-position . none)
-;;           (bottom-divider-width . 1)
-;;           (right-divider-width . 1))))
 (setq doc-view-continuous t)
+(message "doc-view-continuous ON!")
 (setq display-buffer-reuse-frames t)
+(message "display-buffer-reuse-frames ON!")
 ;;(tool-bar-position (quote top))
 (setq window-divider-default-bottom-width 1)
+(message "window-divider-default-bottom-width ON!")
 (setq window-divider-default-right-width 1)
+(message "window-divider-default-right-width ON!")
+(fset (quote yes-or-no-p) t)
+(message "yes-or-no-p ON!")
+(save-place-mode t)
+(message "save-place-mode ON!")
+(recentf-mode 1)
+(message "recentf-mode ON!")
+(setq delete-auto-save-files nil)
+(message "delete-auto-save-files OFF!")
+(setq delete-by-moving-to-trash t)
+(message "delete-by-moving-to-trash ON!")
+(setq auto-save-interval 100)
+(message "auto-save-interval 100!")
+(setq auto-save-visited-file-name t)
+(message "auto-save-visited-file-name ON!")
+(setq command-history-hook nil)
+(message "command-history-hook OFF!")
+(setq completions-file-versions-kept 9992)
+(message "completions-file-versions-kept 9992!")
+(setq history-length 2048)
+(message "history-length 2048!")
+(setq initial-buffer-choice t)
+(message "initial-buffer-choice ON!")
+(setq initial-frame-alist
+      (quote
+       ((tool-bar-position . none)
+	(bottom-divider-width . 1)
+	(right-divider-width . 1))))
+(message "initial-frame-alist ON!")
+(setq list-command-history-max 2048)
+(message "list-command-history-max 2048!")
+(setq undo-ask-before-discard t)
+(message "undo-ask-before-discard ON!")
+(setq undo-limit 9999999999999999)
+(message "undo-limit 9999999999999999!")
+(setq undo-outer-limit 99999999999999999)
+(message "undo-outer-limit 99999999999999999!")
+(setq undo-strong-limit 9999999999999999)
+(message "undo-strong-limit 9999999999999999!")
+(setq user-full-name "Ricardo Malnati")
+(message "user-full-name Ricardo Malnati!")
+(setq user-mail-address "ricardomalnati@gmail.com")
+(message "user-mail-address ricardomalnati@gmail.com!")
+(setq x-gtk-show-hidden-files t)
+(message "x-gtk-show-hidden-files ON!")
+(setq yank-menu-length 32)
+(message "yank-menu-length 32!")
+;; https://docs.projectile.mx/projectile/usage.html
+(projectile-mode +1)
+(message "projectile-mode ON!")
+(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+(setq projectile-project-search-path '("~/projects/" "~/git/" "~/github/" "~/gitlab/"))
+(setq projectile-sort-order 'recentf)
+
+(add-hook 'after-init-hook 'global-company-mode)
+(message "global-company-mode mode globally ON!")
+
+;; https://github.com/arcticicestudio/nord-emacs
+(add-to-list 'custom-theme-load-path (expand-file-name "~/.emacs.d/themes/"))
+(load-theme 'nord t)
+
 (message "\n Custom set for Frame, window, toolbar, menu done! \n")
 ;;--------------------------------------------------------------------
 ;; Loading org-dotemacs
@@ -122,7 +200,7 @@
 (if (file-exists-p emacsti-dotemacslib)
     (load-file emacsti-dotemacslib)
   (message (concat emacsti-dotemacslib " > Lib not found! You have to verify the dotemacslib variable defined in to .emacs file."))
-)
+  )
 (message "Variable emacsti-dotemacslib loaded!")
 (require 'org-dotemacs)
 (setq dotemacs-mode t)
@@ -136,3 +214,9 @@
 ;;--------------------------------------------------------------------
 ;; End .emacs code
 ;;--------------------------------------------------------------------
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
