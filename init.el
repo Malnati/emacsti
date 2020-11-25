822311007f666060b58a6442d57a16cabff96fec
C1316948 Ricardo Malnati Rosa Lima <c1316948@interno.bb.com.br>
2020-11-25 12:21:58 -0300

    adding custom packages lib

--- init.el
+++ init.el
@@ -46,28 +46,8 @@
 (require 'package)
 (package-initialize)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(aggressive-indent-mode nil t)
- '(global-flycheck-mode nil)
- '(package-archives
-   (quote
-    (("marmalade" . "http://marmalade-repo.org/packages/")
-     ("marmalade-sec" . "https://marmalade-repo.org/packages/")
-     ("elpa" . "http://elpa.gnu.org/packages/")
-     ("elpa-sec" . "https://elpa.gnu.org/packages/")
-     ("melpa" . "http://melpa.org/packages/")
-     ("melpa-sec" . "https://melpa.org/packages/"))))
- '(package-enable-at-startup t)
- '(package-selected-packages
-   (quote
-    (nord-theme neotree aggressive-indent yasnippet flycheck which-key projectile hydra company  helm org org-gnome org-dotemacs org-web-tools org-ref org-re-reveal org-presie org-projectile-helm org-projectile org-pomodoro org-pdfview org-page org-mime org-make-toc org-linkany org-index org-edna org-elisp-help org-kanban org-dp org-beautify-theme org-alert)))
- '(which-key-mode t)
- '(which-key-setup-side-window-right-bottom t)
- '(yas-global-mode t))
+(load-file "~/.emacsti/custompackages.el")
+
 ;;(fset (quote yes-or-no-p) t)
 ;;(message "yes-or-no-p ON!")
 
