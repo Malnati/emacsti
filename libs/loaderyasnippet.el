4aa51cb741e50c1caca8922a7aca7f24ae99be07
Ricardo Malnati <ricardomalnati@gmail.com>
2020-11-26 18:22:23 -0300

    adding yasnippet snippets starter

--- libs/loaderyasnippet.el
+++ libs/loaderyasnippet.el
@@ -3,3 +3,8 @@
               "~/.emacsti/libs/yasnippet")
 (require 'yasnippet)
 (yas-global-mode 1)
+
+;; OR, keeping YASnippet defaults try out ~/Downloads/interesting-snippets
+(setq yas-snippet-dirs (append yas-snippet-dirs
+                               '("~/.emacsti/libs/yasnippet-snippets-0.23/snippets")))
+
