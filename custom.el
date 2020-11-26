4c7816f26112a927398920137e534fa5506f735e
Ricardo Malnati <ricardomalnati@gmail.com>
2020-11-26 14:42:26 -0300

    removing stuffs from custom.el

--- custom.el
+++ custom.el
@@ -1,7 +1,3 @@
 ;; add your preferences here
 
-(setq projectile-project-search-path '("~/git/tek/api/"))
-(message "→ projectile search path ~/git/tek/api/")
-(setq projectile-project-search-path '("~/git/tek/mobile/"))
-(message "→ projectile search path ~/git/tek/mobile/")
 
