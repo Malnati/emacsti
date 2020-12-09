f5fa97615e2348ab15c5ca26cd3590bade86a391
Ricardo Malnati <ricardomalnati@gmail.com>
2020-12-09 10:01:14 -0300

    Auto-commited by emacsti-commit-after-save

--- save.el
+++ save.el
@@ -63,12 +63,12 @@
 (setq yank-menu-length 9999)
 (message "→ yank-menu-length 9999!")
 
-(when (eq major-mode 'org-mode)
-  (shell-command-to-string (format "your-script-name %s" buffer-file-name))))
+;;(when (eq major-mode 'org-mode)
+;;  (shell-command-to-string (format "your-script-name %s" buffer-file-name))))
 
 (defun emacsti-commit-after-save ()
        "Commit this buffer to git after save it."
        (interactive)
-       (shell-command-to-string "git add . && git commit -m \""Auto-commited by emacsti-commit-after-save"")
+       (shell-command-to-string "git add . && git commit -m \"Auto-commited by emacsti-commit-after-save\"")
        (message "Exceuted → Auto-commited by emacsti-commit-after-save"))
 (add-hook 'after-save-hook #'emacsti-commit-after-save)
