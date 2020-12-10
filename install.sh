d606ba0a1d24318ca39d471452aaa25468bd575f
Ricardo Malnati <35036961+Malnati@users.noreply.github.com>
2020-12-09 23:29:32 -0300

    adding install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cd ~
+wget https://github.com/Malnati/emacsti/archive/0.0.0-pre-release-1.zip
+sudo chmod +x 0.0.0-pre-release-1.zip
+unzip 0.0.0-pre-release-1.zip
+ln -s ~/emacsti-0.0.0-pre-release-1 ~/.emacsti
+ln -s ~/emacsti-0.0.0-pre-release-1/init.el ~/.emacs
+rm 0.0.0-pre-release-1.zip
