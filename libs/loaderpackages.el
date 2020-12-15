407007d5c5eb2f5aa0e323dd7a466f4b13265c5e
Roberto Mendes <robertomendes.ti@gmail.com>
2020-12-15 00:31:20 -0300

    env(loadpackages): adding libs bind-key use-package ace-window

--- libs/loaderpackages.el
+++ libs/loaderpackages.el
@@ -1,20 +1,7 @@
 (require 'package)
 (package-initialize)
-
-(custom-set-variables
- '(package-archives
-   (quote
-    (("marmalade"     . "http://marmalade-repo.org/packages/")
-     ("marmalade-sec" . "https://marmalade-repo.org/packages/")
-     ("elpa"          . "http://elpa.gnu.org/packages/")
-     ("elpa-sec"      . "https://elpa.gnu.org/packages/")
-     ("melpa"         . "http://melpa.org/packages/")
-     ("melpa-sec"     . "https://melpa.org/packages/"))))
- '(package-enable-at-startup t)
- '(package-selected-packages
-   (quote
-    (hydra helm yasnippet-snippets))))
-
+(load-file "~/.emacsti/libs/bind-key.el")
+(load-file "~/.emacsti/libs/use-package.el")
 (load-file "~/.emacsti/libs/execpathfromshell.el")
 (load-file "~/.emacsti/libs/solarizedemacs.el")
 (load-file "~/.emacsti/libs/powerline.el")
@@ -23,6 +10,7 @@
 (load-file "~/.emacsti/libs/whichkey.el")
 (load-file "~/.emacsti/libs/projectile.el")
 (load-file "~/.emacsti/libs/company.el")
+(load-file "~/.emacsti/libs/ace-window.el")
 ;; (load-file "~/.emacsti/libs/postip.el")
 ;; (load-file "~/.emacsti/libs/quickpeek.el")
 ;; (load-file "~/.emacsti/libs/flycheck.el")
