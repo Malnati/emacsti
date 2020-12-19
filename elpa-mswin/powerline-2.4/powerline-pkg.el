f0f24fcbedfcf64190cac64d224969a0d7a08165
Ricardo Malnati <ricardomalnati@gmail.com>
2020-12-19 20:48:59 -0300

    feat(package): adding mswin elpa site

---
+++ elpa-mswin/powerline-2.4/powerline-pkg.el
@@ -0,0 +1,12 @@
+(define-package "powerline" "2.4" "Rewrite of Powerline"
+  '((cl-lib "0.2"))
+  :commit "d3dcfc57a36111d8e0b037d90c6ffce85ce071b2" :keywords
+  ("mode-line")
+  :authors
+  (("Donald Ephraim Curtis" . "dcurtis@milkbox.net"))
+  :maintainer
+  ("Donald Ephraim Curtis" . "dcurtis@milkbox.net")
+  :url "http://github.com/milkypostman/powerline/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
