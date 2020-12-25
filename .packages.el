283ee93d0b18c36950b7e0a8478bd265913ac2d0
Ricardo Malnati <ricardomalnati@gmail.com>
2020-12-25 20:16:49 -0300

    Auto-commited by emacsti-commit-after-save

--- .packages.el
+++ .packages.el
@@ -210,9 +210,7 @@
 (use-package js2-mode
   :config
   (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
-  (add-to-list 'auto-mode-alist '("\\.js\\'"   . js2-mode))
-  (add-to-list 'auto-mode-alist '("\\.jsx\\'"  . js2-mode))
-  (add-to-list 'auto-mode-alist '("\\.rjsx\\'" . js2-mode)))
+  (add-to-list 'auto-mode-alist '("\\.js\\'"   . js2-mode)))
 
 (use-package js2-refactor
   :after js2-mode)
