c05a0d56df7821da98c0877eec90f24ecb406b4e
Ricardo Malnati <ricardomalnati@gmail.com>
2020-12-25 23:09:38 -0300

    Auto-commited by emacsti-commit-after-save

--- .packages.el
+++ .packages.el
@@ -248,7 +248,29 @@
     (define-key js2-keys (kbd "d") 'json-decrement-number-at-point) ;; Decrement the number at point
     ))
 
-lsp-mode
+(use-package lsp-mode
+    :hook (
+            (js2-mode . lsp)
+            ;; if you want which-key integration
+            (lsp-mode . lsp-enable-which-key-integration))
+    :commands lsp)
+
+;; optionally
+(use-package lsp-ui :commands lsp-ui-mode)
+;; if you are helm user
+(use-package helm-lsp :commands helm-lsp-workspace-symbol)
+;; if you are ivy user
+(use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
+(use-package lsp-treemacs :commands lsp-treemacs-errors-list)
+
+;; optionally if you want to use debugger
+(use-package dap-mode)
+;; (use-package dap-LANGUAGE) to load the dap adapter for your language
+
+;; optional if you want which-key integration
+(use-package which-key
+    :config
+    (which-key-mode))
 
 ;; Starting block for Javascript support
 ;; (load-file "~/.emacsti/libs/xref-js2.el")
