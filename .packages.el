e989e038add54631ff2b82ca5b67a51c08e3ee15
Ricardo Malnati <ricardomalnati@gmail.com>
2020-12-26 20:52:56 -0300

    Auto-commited by emacsti-commit-after-save

--- .packages.el
+++ .packages.el
@@ -233,7 +233,7 @@
     (global-set-key (kbd "M-x") 'helm-M-x)
     (global-set-key (kbd "C-x C-f") #'helm-find-files)))
 
-(setq lsp-keymap-prefix "C-c C-l")
+(setq lsp-keymap-prefix "M-l")
 
 (use-package lsp-mode
   :ensure
