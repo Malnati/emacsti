31db32c62150184603a683cec8beaea0e5cf75e2
Ricardo Malnati <ricardomalnati@gmail.com>
2020-12-26 13:33:59 -0300

    Auto-commited by emacsti-commit-after-save

--- .save.el
+++ .save.el
@@ -69,7 +69,10 @@
   (interactive)
   (shell-command-to-string
    "git add . && git commit -m \"Auto-commited by emacsti-commit-after-save\"")
-  (message "Exceuted → Auto-commited by emacsti-commit-after-save"))
+  (message "Exceuted → Auto-commited by emacsti-commit-after-save")
+  (session-save)
+  (message "Exceuted → Auto-session-save by session-save")
+  )
 
 ;; desktop-auto-save-timeout
 
