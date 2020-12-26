e33716d2c7852e7d18a64cf7a677321bef111cc4
Ricardo Malnati <ricardomalnati@gmail.com>
2020-12-26 13:28:53 -0300

    Auto-commited by emacsti-commit-after-save

--- .save.el
+++ .save.el
@@ -71,40 +71,41 @@
    "git add . && git commit -m \"Auto-commited by emacsti-commit-after-save\"")
   (message "Exceuted → Auto-commited by emacsti-commit-after-save"))
 
-  (progn
-    ;; use only one desktop
-    (setq desktop-path '("~/.emacs.d/"))
-    (setq desktop-dirname "~/.emacs.d/")
-    (setq desktop-base-file-name "emacs-desktop")
-    ;; remove desktop after it's been read
-    (add-hook 'desktop-after-read-hook
-	      '(lambda ()
-		 ;; desktop-remove clears desktop-dirname
-		 (setq desktop-dirname-tmp desktop-dirname)
-		 (desktop-remove)
-		 (setq desktop-dirname desktop-dirname-tmp)))
-    (defun saved-session ()
-      (file-exists-p (concat desktop-dirname "/" desktop-base-file-name)))
-    ;; use session-restore to restore the desktop manually
-    (defun session-restore ()
-      "Restore a saved emacs session."
-      (interactive)
-      (if (saved-session)
-	  (desktop-read)
-	(message "No desktop found.")))
-    ;; use session-save to save the desktop manually
-    (defun session-save ()
-      "Save an emacs session."
-      (interactive)
-      (if (saved-session)
-	  (if (y-or-n-p "Overwrite existing desktop? ")
-	      (desktop-save-in-desktop-dir)
-	    (message "Session not saved."))
-	(desktop-save-in-desktop-dir)))
-    ;; ask user whether to restore desktop at start-up
-    (add-hook 'after-init-hook
-	      '(lambda ()
-		 (if (saved-session)
-		     (if (y-or-n-p "Restore desktop? ")
-			 (session-restore))))))
+;; desktop-auto-save-timeout
+(progn
+  ;; use only one desktop
+  (setq desktop-path '("~/.emacs.d/"))
+  (setq desktop-dirname "~/.emacs.d/")
+  (setq desktop-base-file-name "emacs-desktop")
+  ;; remove desktop after it's been read
+  (add-hook 'desktop-after-read-hook
+	    '(lambda ()
+	       ;; desktop-remove clears desktop-dirname
+	       (setq desktop-dirname-tmp desktop-dirname)
+	       (desktop-remove)
+	       (setq desktop-dirname desktop-dirname-tmp)))
+  (defun saved-session ()
+    (file-exists-p (concat desktop-dirname "/" desktop-base-file-name)))
+  ;; use session-restore to restore the desktop manually
+  (defun session-restore ()
+    "Restore a saved emacs session."
+    (interactive)
+    (if (saved-session)
+	(desktop-read)
+      (message "No desktop found.")))
+  ;; use session-save to save the desktop manually
+  (defun session-save ()
+    "Save an emacs session."
+    (interactive)
+    (if (saved-session)
+	(if (y-or-n-p "Overwrite existing desktop? ")
+	    (desktop-save-in-desktop-dir)
+	  (message "Session not saved."))
+      (desktop-save-in-desktop-dir)))
+  ;; ask user whether to restore desktop at start-up
+  (add-hook 'after-init-hook
+	    '(lambda ()
+	       (if (saved-session)
+		   (if (y-or-n-p "Restore desktop? ")
+		       (session-restore))))))
 
