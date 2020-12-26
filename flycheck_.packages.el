6560e51bca9e49924411d4e022d9d5ee78f801a2
Ricardo Malnati <ricardomalnati@gmail.com>
2020-12-25 21:25:25 -0300

    Auto-commited by emacsti-commit-after-save

--- flycheck_.packages.el
+++ flycheck_.packages.el
@@ -164,7 +164,6 @@
   :config
   (progn
   (projectile-mode +1)
-;;  (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
   (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
   (setq projectile-sort-order 'recentf)))
 
