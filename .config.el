8f4dca70714c2d6333d900c2841145f5f27efb06
Ricardo Malnati <ricardomalnati@gmail.com>
2020-12-27 16:26:47 -0300

    Auto-commited by emacsti-commit-after-save

--- .config.el
+++ .config.el
@@ -97,16 +97,16 @@
 	    (flycheck-mode t)
 	    (setq global-flycheck-mode t)))
 
-(use-package helm  
-  :config (progn
-	    (global-set-key (kbd "M-x") 'helm-M-x)
-	    (global-set-key (kbd "C-x C-f") #'helm-find-files)
-	    (defun helm-buffer-face-mode ()
+(defun helm-buffer-face-mode ()
 	      "Helm buffer face"
 	      (interactive)
 	      (with-helm-buffer
 		(setq line-spacing 2)
 		(buffer-face-set '(:height 87))))
+(use-package helm  
+  :config (progn
+	    (global-set-key (kbd "M-x") 'helm-M-x)
+	    (global-set-key (kbd "C-x C-f") #'helm-find-files)
 	    (add-hook 'helm-update-hook 'helm-buffer-face-mode)))
 
 (use-package helm-projectile  
