4cc7edd90181744a676157bba55df42a78101265
Ricardo Malnati <ricardomalnati@gmail.com>
2020-12-27 14:27:17 -0300

    Auto-commited by emacsti-commit-after-save

--- .config.el
+++ .config.el
@@ -184,7 +184,14 @@
     ;; show the directory of files
     ;; (lsp-ui-peek-show-directory 1)
     ;; enable ‘lsp-ui-peek’
-    (lsp-ui-peek-enable 1)    ;; enable lsp-ui-doc    (lsp-ui-doc-enable 1)    ;; Where to display the doc    (setq lsp-ui-doc-position 1)    ;; Number of seconds before showing the doc    (setq lsp-ui-doc-delay 1)    ;; show diagnostics messages in sideline    (setq lsp-ui-sideline-show-diagnostics 1)    ;; show hover messages in sideline    (setq lsp-ui-sideline-show-hover 1)    ;; show code actions in sideline    (setq lsp-ui-sideline-show-code-actions 1)    ;; When set to 'line' the information will be updated when    ;; user changes current line otherwise the information will    ;; be updated when user changes current point    (setq lsp-ui-sideline-update-mode t)    ;; seconds to wait before showing sideline    (setq lsp-ui-sideline-delay 0)    ;; keystrokes    (define-prefix-command 'lsp)
+    (lsp-ui-peek-enable 1)
+    ;; enable lsp-ui-doc
+    (lsp-ui-doc-enable 1)
+    ;; Where to display the doc
+    (setq lsp-ui-doc-position 1)
+    ;; Number of seconds before showing the doc
+    (setq lsp-ui-doc-delay 1)
+    ;; show diagnostics messages in sideline    (setq lsp-ui-sideline-show-diagnostics 1)    ;; show hover messages in sideline    (setq lsp-ui-sideline-show-hover 1)    ;; show code actions in sideline    (setq lsp-ui-sideline-show-code-actions 1)    ;; When set to 'line' the information will be updated when    ;; user changes current line otherwise the information will    ;; be updated when user changes current point    (setq lsp-ui-sideline-update-mode t)    ;; seconds to wait before showing sideline    (setq lsp-ui-sideline-delay 0)    ;; keystrokes    (define-prefix-command 'lsp)
     (global-set-key (kbd "M-l") 'lsp)
     (define-key lsp (kbd "d") 'lsp-ui-peek-find-definitions)
     (define-key lsp (kbd "r") 'lsp-ui-peek-find-references)
