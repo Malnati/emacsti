1531cef7bbc337915c8485fc96419db1470ce86f
Ricardo Malnati <ricardomalnati@gmail.com>
2020-12-27 14:05:48 -0300

    Auto-commited by emacsti-commit-after-save

--- .config.el
+++ .config.el
@@ -1,95 +1,43 @@
-(when (member window-system '(ns darwin))
-  (use-package exec-path-from-shell
-   
-    :config (progn
-    (exec-path-from-shell-initialize))))
+(when (member window-system '(ns darwin))  (use-package exec-path-from-shell     :config (progn    (exec-path-from-shell-initialize))))
 
-(use-package expand-region
- 
-  :config (progn (global-set-key (kbd "C-=") 'er/expand-region)))
+(use-package expand-region :config (progn (global-set-key (kbd "C-=") 'er/expand-region)))
 
-(use-package multiple-cursors
- 
-  :config (progn
-  (define-prefix-command 'mc)
-  (global-set-key (kbd "C-c m") 'mc)
-  (define-key mc  (kbd "a") 'mc/mark-all-like-this)
-  (define-key mc  (kbd "n") 'mc/mark-next-like-this)
-  (define-key mc  (kbd "p") 'mc/mark-previous-like-this)
-  (global-set-key (kbd "M-<mouse-1>") 'mc/add-cursor-on-click)))
+(use-package multiple-cursors :config (progn  (define-prefix-command 'mc)  (global-set-key (kbd "C-c m") 'mc)  (define-key mc  (kbd "a") 'mc/mark-all-like-this)  (define-key mc  (kbd "n") 'mc/mark-next-like-this)  (define-key mc  (kbd "p") 'mc/mark-previous-like-this)  (global-set-key (kbd "M-<mouse-1>") 'mc/add-cursor-on-click)))
 
 (use-package hydra)
 
-(use-package ivy
-  :after hydra)
+(use-package ivy  :after hydra)
 
-(use-package avy
- 
-  :init
-  :bind ("M-s" . avy-goto-char))
+(use-package avy :init  :bind ("M-s" . avy-goto-char))
 
-(use-package which-key
-  :config (progn
-  (progn
-    ;;(which-key-setup-side-window-right-bottom)
-    (which-key-mode)
-    (with-eval-after-load 'lsp-mode
-      (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration))))
+(use-package which-key  :config (progn  (progn    
 
-(use-package company
- 
-  :config (progn
-  (progn
-    ;; default is 0.2
-    (setq company-minimum-prefix-length 1 company-idle-delay 0.0)
-    (add-hook 'after-init-hook 'global-company-mode)))
+(use-package company  :config (progn  (progn    ;; default is 0.2    (setq company-minimum-prefix-length 1 company-idle-delay 0.0)    (add-hook 'after-init-hook 'global-company-mode)))
 
 (use-package dap-mode)
 
 (use-package treemacs)
 
-(use-package treemacs-projectile
- 
-  :after treemacs
-  :after lsp-mode)
+(use-package treemacs-projectile  :after treemacs  :after lsp-mode)
 
-(use-package nord-theme
- 
-  :config (progn
-  (add-to-list 'custom-theme-load-path
-	       (expand-file-name "~/.emacs.d/themes/"))
-  (load-theme 'nord t))
+(use-package nord-theme  :config (progn  (add-to-list 'custom-theme-load-path
+	       (expand-file-name "~/.emacs.d/themes/"))  (load-theme 'nord t))
 
-(use-package s
- )
+(use-package s )
 
-(use-package memoize
- )
+(use-package memoize )
 
-(use-package dash
- )
+(use-package dash )
 
-(use-package all-the-icons
- )
+(use-package all-the-icons )
 
-(use-package all-the-icons-ibuffer
- )
+(use-package all-the-icons-ibuffer )
 
-(use-package company-box
- 
-  :after company-mode
-  :after lsp-mode
-  :hook (company-mode . company-box-mode)
-  :config (progn
-  (progn
-    (setq company-box-icons-unknown 'fa_question_circle)
-    (setq company-box-icons-elisp
+(use-package company-box  :after company-mode  :after lsp-mode  :hook (company-mode . company-box-mode)  :config (progn  (progn    (setq company-box-icons-unknown 'fa_question_circle)    (setq company-box-icons-elisp
 	  '((fa_tag :face font-lock-function-name-face) ;; Function
 	    (fa_cog :face font-lock-variable-name-face) ;; Variable
 	    (fa_cube :face font-lock-constant-face) ;; Feature
-	    (md_color_lens :face font-lock-doc-face))) ;; Face
-    (setq company-box-icons-yasnippet 'fa_bookmark)
-    (setq company-box-icons-lsp
+	    (md_color_lens :face font-lock-doc-face))) ;; Face    (setq company-box-icons-yasnippet 'fa_bookmark)    (setq company-box-icons-lsp
 	  '((1 . fa_text_height) ;; Text
 	    (2 . (fa_tags :face font-lock-function-name-face)) ;; Method
 	    (3 . (fa_tag :face font-lock-function-name-face)) ;; Function
@@ -117,74 +65,24 @@
 	    (25 . fa_arrows)) ;; TypeParameter
 	  )))
 
-(use-package indent-guide
- 
-  :config (progn
-  (indent-guide-global-mode)
-  (setq indent-guide-char "|"))
+(use-package indent-guide  :config (progn  (indent-guide-global-mode)  (setq indent-guide-char "|"))
 
-(use-package drag-stuff
- 
-  :config (progn
-  (progn
-    (drag-stuff-global-mode 1)
-    (drag-stuff-define-keys)
-    (global-set-key [(meta up)]   'drag-stuff-up)
-    (global-set-key [(meta down)] 'drag-stuff-down)))
+(use-package drag-stuff  :config (progn  (progn    (drag-stuff-global-mode 1)    (drag-stuff-define-keys)    (global-set-key [(meta up)]   'drag-stuff-up)    (global-set-key [(meta down)] 'drag-stuff-down)))
 
-(use-package projectile
- 
-  :config (progn
-  (progn
-    (projectile-mode +1)
-    (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
-    (setq projectile-sort-order 'recentf)))
+(use-package projectile  :config (progn  (progn    (projectile-mode +1)    (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)    (setq projectile-sort-order 'recentf)))
 
-(use-package yasnippet
- 
-  :config (progn
-  (progn
-    (yas-reload-all)
-    (add-hook 'prog-mode-hook #'yas-minor-mode)
-    (yas-global-mode 1)))
+(use-package yasnippet  :config (progn  (progn    (yas-reload-all)    (add-hook 'prog-mode-hook #'yas-minor-mode)    (yas-global-mode 1)))
 
-(use-package yasnippet-snippets
- 
-  :after yasnippet
-  :config (progn
-  (progn
-    (when (member window-system '(pc w32 ms-dos windows-nt cygwin))
-      (setq yas-snippet-dirs
-	    (append yas-snippet-dirs '("~/.emacs.d/libs/elpa-mswin/yasnippet-snippets-20201221.849/snippets"))))
-    (when (member system-type '(gnu/linux gnu x))
-      (setq yas-snippet-dirs
-	    (append yas-snippet-dirs '("~/.emacs.d/libs/elpa-linux/yasnippet-classic-snippets-1.0.2")))
-      (setq yas-snippet-dirs
-            (append yas-snippet-dirs '("~/.emacs.d/libs/elpa-linux/snippet-20130210.2315"))))))
+(use-package yasnippet-snippets  :after yasnippet  :config (progn  (progn    (when (member window-system '(pc w32 ms-dos windows-nt cygwin))      (setq yas-snippet-dirs
+	    (append yas-snippet-dirs '("~/.emacs.d/libs/elpa-mswin/yasnippet-snippets-20201221.849/snippets"))))    (when (member system-type '(gnu/linux gnu x))      (setq yas-snippet-dirs
+	    (append yas-snippet-dirs '("~/.emacs.d/libs/elpa-linux/yasnippet-classic-snippets-1.0.2")))      (setq yas-snippet-dirs            (append yas-snippet-dirs '("~/.emacs.d/libs/elpa-linux/snippet-20130210.2315"))))))
 
-(use-package flycheck
- 
-  :config (progn
-  (flycheck-mode t)
-  (setq global-flycheck-mode t))
+(use-package flycheck  :config (progn  (flycheck-mode t)  (setq global-flycheck-mode t))
 
-(use-package helm
- 
-  :config (progn
-  (progn
-    (global-set-key (kbd "M-x") 'helm-M-x)
-    (global-set-key (kbd "C-x C-f") #'helm-find-files)))
+(use-package helm  :config (progn  (progn    (global-set-key (kbd "M-x") 'helm-M-x)    (global-set-key (kbd "C-x C-f") #'helm-find-files)))
 
-(progn
-  (setq lsp-keymap-prefix "M-l")
-
-  (use-package lsp-mode
-   
-    :hook
-    ((js2-mode . lsp-mode)
-     (lsp-mode . lsp-enable-which-key-integration))
-    :commands lsp
-    :config (progn (progn
+(progn  (setq lsp-keymap-prefix "M-l")
+  (use-package lsp-mode     :hook    ((js2-mode . lsp-mode)     (lsp-mode . lsp-enable-which-key-integration))  :commands lsp  :config (progn (progn
 	      ;; Enable/disable snippet completion support.
 	      (setq lsp-enable-snippet t)
 	      ;; Display all of the info returned by document/onHover.
@@ -236,137 +134,32 @@
 	      (setq lsp-headerline-breadcrumb-enable 1)
 	      ;; (with-eval-after-load 'lsp-mode
 	      ;;   (mapc #'lsp-flycheck-add-mode '(js2-mode)))
-	      ))
-  )
-(use-package lsp-ui
- 
-  :after lsp-mode
-  :config (progn
-  (progn
-    ;; show the directory of files
-    ;; (lsp-ui-peek-show-directory 1)
-    ;; enable ‘lsp-ui-peek’
-    (lsp-ui-peek-enable 1)
-    ;; enable lsp-ui-doc
-    (lsp-ui-doc-enable 1)
-    ;; Where to display the doc
-    (setq lsp-ui-doc-position 1)
-    ;; Number of seconds before showing the doc
-    (setq lsp-ui-doc-delay 1)
-    ;; show diagnostics messages in sideline
-    (setq lsp-ui-sideline-show-diagnostics 1)
-    ;; show hover messages in sideline
-    (setq lsp-ui-sideline-show-hover 1)
-    ;; show code actions in sideline
-    (setq lsp-ui-sideline-show-code-actions 1)
-    ;; When set to 'line' the information will be updated when
-    ;; user changes current line otherwise the information will
-    ;; be updated when user changes current point
-    (setq lsp-ui-sideline-update-mode t)
-    ;; seconds to wait before showing sideline
-    (setq lsp-ui-sideline-delay 0)
-    ;; keystrokes
-    (define-prefix-command 'lsp)
-    (global-set-key (kbd "M-l") 'lsp)
-    (define-key lsp (kbd "d") 'lsp-ui-peek-find-definitions)
-    (define-key lsp (kbd "r") 'lsp-ui-peek-find-references)
-    (define-key lsp (kbd "i") 'lsp-ui-peek-find-implementation)
-    (define-key lsp (kbd "w") 'lsp-ui-peek-find-workspace-symbol)
-    (define-key lsp (kbd "<left>") 'lsp-ui-peek-jump-backward)
-    (define-key lsp (kbd "<right>") 'lsp-ui-peek-jump-forward)
-    )
-  )
+	      ))  )
+(use-package lsp-ui  :after lsp-mode  :config (progn  (progn    ;; show the directory of files    ;; (lsp-ui-peek-show-directory 1)    ;; enable ‘lsp-ui-peek’    (lsp-ui-peek-enable 1)    ;; enable lsp-ui-doc    (lsp-ui-doc-enable 1)    ;; Where to display the doc    (setq lsp-ui-doc-position 1)    ;; Number of seconds before showing the doc    (setq lsp-ui-doc-delay 1)    ;; show diagnostics messages in sideline    (setq lsp-ui-sideline-show-diagnostics 1)    ;; show hover messages in sideline    (setq lsp-ui-sideline-show-hover 1)    ;; show code actions in sideline    (setq lsp-ui-sideline-show-code-actions 1)    ;; When set to 'line' the information will be updated when    ;; user changes current line otherwise the information will    ;; be updated when user changes current point    (setq lsp-ui-sideline-update-mode t)    ;; seconds to wait before showing sideline    (setq lsp-ui-sideline-delay 0)    ;; keystrokes    (define-prefix-command 'lsp)    (global-set-key (kbd "M-l") 'lsp)    (define-key lsp (kbd "d") 'lsp-ui-peek-find-definitions)    (define-key lsp (kbd "r") 'lsp-ui-peek-find-references)    (define-key lsp (kbd "i") 'lsp-ui-peek-find-implementation)    (define-key lsp (kbd "w") 'lsp-ui-peek-find-workspace-symbol)    (define-key lsp (kbd "<left>") 'lsp-ui-peek-jump-backward)    (define-key lsp (kbd "<right>") 'lsp-ui-peek-jump-forward)    )  )
 
-(use-package helm-lsp
- 
-  :after helm
-  :after lsp-mode
-  :config (progn
-  (progn
-    (define-key lsp-mode-map
-      [remap xref-find-apropos]
-      #'helm-lsp-workspace-symbol)))
+(use-package helm-lsp  :after helm  :after lsp-mode  :config (progn  (progn    (define-key lsp-mode-map      [remap xref-find-apropos]      #'helm-lsp-workspace-symbol)))
 
-(use-package helm-projectile
- 
-  :config (progn (progn
+(use-package helm-projectile  :config (progn (progn
 	    (helm-projectile-on)))
 
-(use-package js2-mode
- 
-  :config (progn
-  (progn
-    (define-prefix-command 'js2-keys)
-    (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
-    (add-to-list 'auto-mode-alist '("\\.js\\'"   . js2-mode))))
+(use-package js2-mode  :config (progn  (progn    (define-prefix-command 'js2-keys)    (add-to-list 'interpreter-mode-alist '("node" . js2-mode))    (add-to-list 'auto-mode-alist '("\\.js\\'"   . js2-mode))))
 
-(use-package js2-refactor
- 
-  :after js2-mode)
+(use-package js2-refactor  :after js2-mode)
 
-(when (member system-type '(gnu/linux gnu x))
-  (use-package xref-js2
-   
-    :config (progn
-    (progn
-      (define-prefix-command 'xref-js2)
-      (global-set-key (kbd "C-c x") 'xref-js2)
-      (define-key xref-js2 (kbd "a")   'js2-mode-show-all)
-      (define-key xref-js2 (kbd "e")   'js2-mode-hide-element)
-      (define-key xref-js2 (kbd "f")   'js2-mode-toggle-hide-functions)
-      (define-key xref-js2 (kbd "g")   'xref-revert-buffer)
-      (define-key xref-js2 (kbd "j")   'js2-jump-to-definition)
-      (define-key xref-js2 (kbd "n")   'xref-next-line)
-      (define-key xref-js2 (kbd "N")   'xref-next-group)
-      (define-key xref-js2 (kbd "o")   'js2-mode-toggle-element)
-      (define-key xref-js2 (kbd "C-o") 'xref-show-location-at-point)
-      (define-key xref-js2 (kbd "P")   'xref-prev-group)
-      (define-key xref-js2 (kbd "p")   'xref-prev-line)
-      (define-key xref-js2 (kbd "r")   'xref-query-replace-in-results)
-      (define-key xref-js2 (kbd "s")   'js2-mode-show-element)
-      (define-key xref-js2 (kbd "t")   'js2-mode-toggle-hide-comments)
-      (define-key xref-js2 (kbd "w")   'js2-mode-toggle-warnings-and-errors)
-      (define-key xref-js2 (kbd "RET") 'xref-goto-xref)
-      (define-key xref-js2 (kbd "TAB") 'xref-quit-and-goto-xref)
-      (define-key xref-js2 (kbd ".")   'xref-next-line)
-      (define-key xref-js2 (kbd ",")   'xref-prev-line)
-      ;;(define-key xref-js2 [remap indent-new-comment-line] #'js2-line-break)
-      ;;(define-key xref-js2 [down-mouse-3] #'js2-down-mouse-3)
-      (add-hook 'js2-mode-hook (lambda ()
+(when (member system-type '(gnu/linux gnu x))  (use-package xref-js2     :config (progn    (progn      (define-prefix-command 'xref-js2)      (global-set-key (kbd "C-c x") 'xref-js2)      (define-key xref-js2 (kbd "a")   'js2-mode-show-all)      (define-key xref-js2 (kbd "e")   'js2-mode-hide-element)      (define-key xref-js2 (kbd "f")   'js2-mode-toggle-hide-functions)      (define-key xref-js2 (kbd "g")   'xref-revert-buffer)      (define-key xref-js2 (kbd "j")   'js2-jump-to-definition)      (define-key xref-js2 (kbd "n")   'xref-next-line)      (define-key xref-js2 (kbd "N")   'xref-next-group)      (define-key xref-js2 (kbd "o")   'js2-mode-toggle-element)      (define-key xref-js2 (kbd "C-o") 'xref-show-location-at-point)      (define-key xref-js2 (kbd "P")   'xref-prev-group)      (define-key xref-js2 (kbd "p")   'xref-prev-line)      (define-key xref-js2 (kbd "r")   'xref-query-replace-in-results)      (define-key xref-js2 (kbd "s")   'js2-mode-show-element)      (define-key xref-js2 (kbd "t")   'js2-mode-toggle-hide-comments)      (define-key xref-js2 (kbd "w")   'js2-mode-toggle-warnings-and-errors)      (define-key xref-js2 (kbd "RET") 'xref-goto-xref)      (define-key xref-js2 (kbd "TAB") 'xref-quit-and-goto-xref)      (define-key xref-js2 (kbd ".")   'xref-next-line)      (define-key xref-js2 (kbd ",")   'xref-prev-line)      ;;(define-key xref-js2 [remap indent-new-comment-line] #'js2-line-break)      ;;(define-key xref-js2 [down-mouse-3] #'js2-down-mouse-3)      (add-hook 'js2-mode-hook (lambda ()
 				 (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t))))))
 
-(use-package json-mode
- 
-  :after js2-mode
-  :config (progn
-  (progn
-    (add-to-list 'auto-mode-alist '("\\.json\\'"   . json-mode))
-    (global-set-key (kbd "C-c C-j") 'js2-keys)
-    ;; format the region/buffer with json-reformat (https://github.com/gongo/json-reformat)
-    ;; (define-key js2-keys (kbd "f") 'json-mode-beautify)
-    ;; display a path to the object at point with json-snatcher (https://github.com/Sterlingg/json-snatcher)
-    (define-key js2-keys (kbd "p") 'json-mode-show-path)
-    ;; copy a path to the object at point to the kill ring with json-snatcher (https://github.com/Sterlingg/json-snatcher)
-    (define-key js2-keys (kbd "P") 'json-mode-kill-path)
-    ;; Toggle between true and false at point
-    ;; (define-key js2-keys (kbd "t") 'json-toggle-boolean)
-    ;; sexp Replace the at point with null
-    ;; (define-key js2-keys (kbd "k") 'json-nullify-sexp)
-    ;; Increment the number at point
-    ;; (define-key js2-keys (kbd "i") 'json-increment-number-at-point)
-    ;; Decrement the number at point
-    ;; (define-key js2-keys (kbd "d") 'json-decrement-number-at-point)
-    ))
+(use-package json-mode  :after js2-mode  :config (progn  (progn    (add-to-list 'auto-mode-alist '("\\.json\\'"   . json-mode))    (global-set-key (kbd "C-c C-j") 'js2-keys)    ;; format the region/buffer with json-reformat (https://github.com/gongo/json-reformat)    ;; (define-key js2-keys (kbd "f") 'json-mode-beautify)    ;; display a path to the object at point with json-snatcher (https://github.com/Sterlingg/json-snatcher)    (define-key js2-keys (kbd "p") 'json-mode-show-path)    ;; copy a path to the object at point to the kill ring with json-snatcher (https://github.com/Sterlingg/json-snatcher)    (define-key js2-keys (kbd "P") 'json-mode-kill-path)    ;; Toggle between true and false at point    ;; (define-key js2-keys (kbd "t") 'json-toggle-boolean)    ;; sexp Replace the at point with null    ;; (define-key js2-keys (kbd "k") 'json-nullify-sexp)    ;; Increment the number at point    ;; (define-key js2-keys (kbd "i") 'json-increment-number-at-point)    ;; Decrement the number at point    ;; (define-key js2-keys (kbd "d") 'json-decrement-number-at-point)    ))
 
 ;; (use-package spaceline
 ;;  
-;;   :after s
-;;   :after memoize
-;;   :config (progn
+;; :after s
+;; :after memoize
+;; :config (progn
 ;;   (use-package powerline
 ;;    )
 ;;   (use-package spaceline-config
-;;     :config (progn
+;;   :config (progn
 ;;     (spaceline-toggle-buffer-modified-on)
 ;;     (spaceline-toggle-line-column-on)
 ;;     (spaceline-toggle-minor-modes-off)
@@ -381,13 +174,13 @@
 ;;     (spaceline-emacs-theme 'date 'time)
 ;;     (use-package spaceline-all-the-icons
 ;;      
-;;       :after spaceline
-;;       :config (progn
+;;     :after spaceline
+;;     :config (progn
 ;;       (spaceline-all-the-icons-theme)
 ;;       (spaceline-all-the-icons--setup-git-ahead))))
 
 ;; (use-package desktop
-;;   :config (progn
+;; :config (progn
 ;;   (progn
 ;;     ;; use only one desktop
 ;;     (setq desktop-path '("~/.emacs.d/"))
@@ -463,7 +256,7 @@
 
 
 ;; (use-package js2-highlight-vars
-;;   :config (progn
+;; :config (progn
 ;;   (js2-highlight-vars-mode t)
 ;;   (add-to-list 'auto-mode-alist '("\\.js\\'"   . js2-highlight-vars-mode))
 ;;   (add-to-list 'auto-mode-alist '("\\.jsx\\'"  . js2-highlight-vars-mode))
