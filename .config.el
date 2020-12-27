d80b667d4d9780cd84a9f44c221baa68e8260727
Ricardo Malnati <ricardomalnati@gmail.com>
2020-12-27 14:12:47 -0300

    Auto-commited by emacsti-commit-after-save

--- .config.el
+++ .config.el
@@ -71,31 +71,22 @@
 	    (2 . (fa_tags :face font-lock-function-name-face)) ;; Method
 	    (3 . (fa_tag :face font-lock-function-name-face)) ;; Function
 	    (4 . (fa_tag :face font-lock-function-name-face)) ;; Constructor
-	    (5 . (fa_cog
-:foreground "#FF9800")) ;; Field
-	    (6 . (fa_cog
-:foreground "#FF9800")) ;; Variable
-	    (7 . (fa_cube
-:foreground "#7C4DFF")) ;; Class
-	    (8 . (fa_cube
-:foreground "#7C4DFF")) ;; Interface
-	    (9 . (fa_cube
-:foreground "#7C4DFF")) ;; Module
-	    (10 . (fa_cog
-:foreground "#FF9800")) ;; Property
+	    (5 . (fa_cog :foreground "#FF9800")) ;; Field
+	    (6 . (fa_cog :foreground "#FF9800")) ;; Variable
+	    (7 . (fa_cube :foreground "#7C4DFF")) ;; Class
+	    (8 . (fa_cube :foreground "#7C4DFF")) ;; Interface
+	    (9 . (fa_cube :foreground "#7C4DFF")) ;; Module
+	    (10 . (fa_cog :foreground "#FF9800")) ;; Property
 	    (11 . md_settings_system_daydream) ;; Unit
-	    (12 . (fa_cog
-:foreground "#FF9800")) ;; Value
+	    (12 . (fa_cog :foreground "#FF9800")) ;; Value
 	    (13 . (md_storage :face font-lock-type-face)) ;; Enum
-	    (14 . (md_closed_caption
-:foreground "#009688")) ;; Keyword
+	    (14 . (md_closed_caption :foreground "#009688")) ;; Keyword
 	    (15 . md_closed_caption) ;; Snippet
 	    (16 . (md_color_lens :face font-lock-doc-face)) ;; Color
 	    (17 . fa_file_text_o) ;; File
 	    (18 . md_refresh) ;; Reference
 	    (19 . fa_folder_open) ;; Folder
-	    (20 . (md_closed_caption
-:foreground "#009688")) ;; EnumMember
+	    (20 . (md_closed_caption :foreground "#009688")) ;; EnumMember
 	    (21 . (fa_square :face font-lock-constant-face)) ;; Constant
 	    (22 . (fa_cube :face font-lock-type-face)) ;; Struct
 	    (23 . fa_calendar) ;; Event
