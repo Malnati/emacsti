efd3c13e59970e04e4b1ad76aac63589d4d6736d
Ricardo Malnati <ricardomalnati@gmail.com>
2020-12-27 14:56:15 -0300

    Auto-commited by emacsti-commit-after-save

--- .config.el
+++ .config.el
@@ -26,7 +26,6 @@
 (use-package which-key
   :config  (progn
 	     (which-key-mode)
-	     ;;(which-key-setup-side-window-right-bottom)
 	     (with-eval-after-load 'lsp-mode
 	       (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration))))
 
