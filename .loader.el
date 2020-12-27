8ee9855e00de59e5fcb46a9987edf871e452a728
Ricardo Malnati <ricardomalnati@gmail.com>
2020-12-27 15:00:07 -0300

    Auto-commited by emacsti-commit-after-save

--- .loader.el
+++ .loader.el
@@ -2,7 +2,9 @@
 ;;; Commentary:
 ;;; Code:
 
-(load-file "~/.emacs.d/.packages.el")
+;; (load-file "~/.emacs.d/.packages.el")
+(load-file "~/.emacs.d/.install.el")
+(load-file "~/.emacs.d/.config.el")
 (load-file "~/.emacs.d/.faces.el")
 (load-file "~/.emacs.d/.save.el")
 (load-file "~/.emacs.d/.custom.el")
