67636c092a5bf734975fb03433738ddd8455793c
Ricardo Malnati <ricardomalnati@gmail.com>
2020-12-27 13:43:28 -0300

    Auto-commited by emacsti-commit-after-save

---
+++ libs/elpa-linux/flow-minor-mode-20200905.1730/flow-minor-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flow-minor-mode" "20200905.1730" "Flow type mode based on web-mode." '((emacs "25.1")) :commit "804217a15a28f6918fba93c91d495ed7d50b0495" :url "https://github.com/an-sh/flow-minor-mode")
