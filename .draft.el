17afff37e41c4e020feea94f11d0da554f2c37c4
Ricardo Malnati <ricardomalnati@gmail.com>
2020-12-27 22:46:56 -0300

    Auto-commited by emacsti-commit-after-save

--- .draft.el
+++ .draft.el
@@ -60,14 +60,15 @@
 	      (setenv "CLASSPATH" (concat cp ":" "~/.emacs.d/servers/eclipse.jdt.ls-0.67.0/target"))
 	      (unwind-protect (eglot--eclipse-jdt-contact nil)
 		(setenv "CLASSPATH" cp)))
+	    (setcdr (assq 'java-mode eglot-server-programs) #'my-eglot-eclipse-jdt-contact)
+	    (add-hook 'java-mode-hook 'eglot-ensure)
 	    (add-to-list 'eglot-server-programs
 			 '(js2-mode . ("node" "~/.emacs.d/servers/typescript-language-server/lib/language-server-stdio")))
 	    (add-to-list 'eglot-server-programs
 			 '(js2-mode . ("node" "~/.emacs.d/servers/typescript-language-server/lib/language-server-stdio")))))
 
   
-(setcdr (assq 'java-mode eglot-server-programs) #'my-eglot-eclipse-jdt-contact)
-(add-hook 'java-mode-hook 'eglot-ensure)
+
 
 ;; java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=1044 -Declipse.application=org.eclipse.jdt.ls.core.id1 -Dosgi.bundles.defaultStartLevel=4 -Declipse.product=org.eclipse.jdt.ls.core.product -Dlog.level=ALL -noverify -Xmx1G -jar ./plugins/org.eclipse.equinox.launcher_1.5.200.v20180922-1751.jar -configuration ./config_linux -data /path/to/data --add-modules=ALL-SYSTEM --add-opens java.base/java.util=ALL-UNNAMED --add-opens java.base/java.lang=ALL-UNNAMED
 
