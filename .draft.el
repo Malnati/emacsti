aa9c41c426b1c66386e4ecfda13314323f48f21b
Ricardo Malnati <ricardomalnati@gmail.com>
2020-12-27 23:40:21 -0300

    Auto-commited by emacsti-commit-after-save

--- .draft.el
+++ .draft.el
@@ -67,7 +67,7 @@
 	      (setcdr (assq 'java-mode eglot-server-programs) #'my-eglot-eclipse-jdt-contact)
 	      (add-hook 'java-mode-hook 'eglot-ensure))))
 
-;; java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=1044 -Declipse.application=org.eclipse.jdt.ls.core.id1 -Dosgi.bundles.defaultStartLevel=4 -Declipse.product=org.eclipse.jdt.ls.core.product -Dlog.level=ALL -noverify -Xmx1G -jar ./plugins/org.eclipse.equinox.launcher_1.6.0.v20200915-1508.jar -configuration ./config_linux -data ~/.emacs.d/tmp/data --add-modules=ALL-SYSTEM --add-opens java.base/java.util=ALL-UNNAMED --add-opens java.base/java.lang=ALL-UNNAMED
+;; java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=1044 -Declipse.application=org.eclipse.jdt.ls.core.id1 -Dosgi.bundles.defaultStartLevel=4 -Declipse.product=org.eclipse.jdt.ls.core.product -Dlog.level=ALL -noverify -Xmx1G -jar ~/.emacs.d/servers/eclipse.jdt.ls-0.67.0/org.eclipse.jdt.ls.product/target/repository/plugins/org.eclipse.equinox.launcher_1.6.0.v20200915-1508.jar -configuration ./config_linux -data ~/.emacs.d/tmp/data --add-modules=ALL-SYSTEM --add-opens java.base/java.util=ALL-UNNAMED --add-opens java.base/java.lang=ALL-UNNAMED
 
 ;; (use-package spaceline
 ;;   :ensure
