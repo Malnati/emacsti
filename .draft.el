990ce65b51da8f8d49a54a83874ceec12a07967f
Ricardo Malnati <ricardomalnati@gmail.com>
2020-12-27 22:52:26 -0300

    Auto-commited by emacsti-commit-after-save

--- .draft.el
+++ .draft.el
@@ -65,7 +65,7 @@
 		(setenv "CLASSPATH" cp)))
 	    (setcdr (assq 'java-mode eglot-server-programs) #'my-eglot-eclipse-jdt-contact)
 	    (add-to-list 'eglot-server-programs
-			 '(js2-mode . ("~/.emacs.d/servers/typescript-language-server" "--stdio")))))
+			 '(js2-mode . ("~/.emacs.d/servers/javascript-typescript-langserver" "--stdio")))))
 ;; java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=1044 -Declipse.application=org.eclipse.jdt.ls.core.id1 -Dosgi.bundles.defaultStartLevel=4 -Declipse.product=org.eclipse.jdt.ls.core.product -Dlog.level=ALL -noverify -Xmx1G -jar ./plugins/org.eclipse.equinox.launcher_1.5.200.v20180922-1751.jar -configuration ./config_linux -data /path/to/data --add-modules=ALL-SYSTEM --add-opens java.base/java.util=ALL-UNNAMED --add-opens java.base/java.lang=ALL-UNNAMED
 
 ;; (use-package spaceline
