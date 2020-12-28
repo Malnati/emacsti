cb311fc245bf4185296ddb1607248a3951127ee8
Ricardo Malnati <ricardomalnati@gmail.com>
2020-12-27 23:09:54 -0300

    Auto-commited by emacsti-commit-after-save

--- .loader.el
+++ .loader.el
@@ -26,11 +26,11 @@
   (require 'use-package))
 
 ;;(load-file "~/.emacs.d/.install.el")
-(load-file "~/.emacs.d/.draft.el")
 (load-file "~/.emacs.d/.config.el")
 (load-file "~/.emacs.d/.defer.el")
 (load-file "~/.emacs.d/.faces.el")
 (load-file "~/.emacs.d/.save.el")
 (load-file "~/.emacs.d/.custom.el")
+(load-file "~/.emacs.d/.draft.el")
 
 ;;; #.loader.le# ends here
